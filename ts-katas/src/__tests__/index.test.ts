ccb84f8997887523d76c82de1cda63caa531bd7e
SoderbergHampus <soderberg.dev@gmail.com>
2024-02-15 21:38:09 +0100

    A few katas

--- ts-katas/src/__tests__/index.test.ts
+++ ts-katas/src/__tests__/index.test.ts
@@ -1,7 +1,40 @@
-import {helloWorld} from '../index';
+import {helloWorld, number} from '../index';
 
 describe('Test should be able to test the functions in index', () => {
   it('Should return "Hello World"', () => {
     expect(helloWorld()).toEqual('Hello World');
+  });
+});
+
+describe('Keep track of number of people on the bus', () => {
+  it('Should return correct number of people', () => {
+    expect(
+      number([
+        [10, 0],
+        [3, 5],
+        [5, 8],
+      ])
+    ).toEqual(5);
+    expect(
+      number([
+        [3, 0],
+        [9, 1],
+        [4, 10],
+        [12, 2],
+        [6, 1],
+        [7, 10],
+      ])
+    ).toEqual(17);
+    expect(
+      number([
+        [3, 0],
+        [9, 1],
+        [4, 8],
+        [12, 2],
+        [6, 1],
+        [7, 8],
+      ])
+    ).toEqual(21);
+    expect(number([[0, 0]])).toEqual(0);
   });
 });
--- ts-katas/src/index.ts
+++ ts-katas/src/index.ts
@@ -3,3 +3,55 @@ export const helloWorld = (): string => {
 };
 
 console.log(helloWorld());
+
+/**
+ * Function to keep track of the number of people on a bus. Must be >= 0.
+ *
+ * @param busStops Array of integer pairs, index 0 is people getting on and
+ * index 1 are people getting off.
+ */
+export function number(busStops: [number, number][]): number {
+  let sum = 0;
+  busStops.map((pair) => pair[0] - pair[1]).forEach((ele) => (sum += ele));
+  return sum;
+}
+
+console.log(
+  '\nShould be 17: ',
+  number([
+    [3, 0],
+    [9, 1],
+    [4, 10],
+    [12, 2],
+    [6, 1],
+    [7, 10],
+  ])
+);
+
+/**
+ * Highest scoring word, score is spot in alphabet
+ */
+
+export const highestScoringWord = (str: string): string => {
+  const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+  const words = str.split(' ');
+  let leadingScore = 0;
+  let highestScoringWord = '';
+  for (let i in words) {
+    let wordScore: number = words[i]
+      .split('')
+      .map((ele) => alphabet.indexOf(ele) + 1)
+      .reduce((a, b) => a + b);
+
+    if (wordScore > leadingScore) {
+      highestScoringWord = words[i];
+      leadingScore = wordScore;
+    }
+  }
+  return highestScoringWord;
+};
+
+console.log(
+  '\nShoule be "taxi":',
+  highestScoringWord('man i need a taxi up to ubud')
+);
